1ada686e16609e230eec2b51ac6ca5ebf75504fc
Sheetal <88383407+sheetal59@users.noreply.github.com>
2023-10-23 21:02:36 +0530

    Time: 1 ms (76.31%) | Memory: 40 MB (10.22%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for(int i=0; i<=15; i++){
+            int power = (int) Math.pow(4,i);
+            if(power==n){
+                return true;
+                
+            }
+            if(power>n){
+                return false;
+            }
+
+        }
+        return false;
+    }
+}
