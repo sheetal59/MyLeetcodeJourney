9723bbbb3feac59a7359bc7323cd4303836aa41e
Sheetal <88383407+sheetal59@users.noreply.github.com>
2023-11-08 15:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (93.16%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.java
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.java
@@ -4,9 +4,7 @@ class Solution {
         if(sx==fx && sy==fy){
             return t!=1;
         }
-       // if(t==1){
-         //   return true;
-        //}
+       
         //The chebysev distance is-- max(x2-x1, y2-y1)
        int distToReach = Math.max(Math.abs(sx-fx), Math.abs(sy-fy));
         //condition
