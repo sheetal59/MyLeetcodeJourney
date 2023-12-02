ce3c7f12fed01ea1247528ef537e2b6523ab3268
Sheetal <88383407+sheetal59@users.noreply.github.com>
2023-12-02 23:49:04 +0530

    Time: 68 ms (17.46%) | Memory: 61.8 MB (26.38%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,35 @@
+class Solution {
+    class Pair implements Comparable<Pair>{
+        int val;
+        int idx;
+
+        public Pair(int val, int idx){
+            this.val = val;
+            this.idx = idx;
+        }
+        @Override
+        public int compareTo(Pair p2){
+            return p2.val - this.val;
+        }
+    }
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int ans[] = new int[nums.length-k+1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        //First window
+        for(int i=0; i<k; i++){
+            pq.add(new Pair(nums[i], i));
+        }
+        ans[0] = pq.peek().val;
+
+        //remaining windows
+        for(int i=k; i<nums.length; i++){
+            while(pq.size()>0 && pq.peek().idx <= (i-k)){
+                pq.remove();
+            }
+            pq.add(new Pair(nums[i], i));
+            ans[i-k+1] = pq.peek().val;
+        }
+        return ans;
+    }
+}
