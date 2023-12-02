b18de2a316c4c237cbede31e01e52b5252887deb
Sheetal <88383407+sheetal59@users.noreply.github.com>
2023-12-02 20:46:31 +0530

    Time: 3 ms (48.17%) | Memory: 44.8 MB (15.72%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,44 +1,19 @@
 class Solution {
-    class Pair{
-        int key;
-        int val;
-
-        public Pair(int key, int val){
-            this.key = key;
-            this.val = val;
-        }
-    }
-
-    class PairComparator implements Comparator<Pair>{
-        public int compare(Pair p1, Pair p2){
-            Integer key1 = p1.key;
-            Integer key2 = p2.key;
-            Integer val1 = p1.val;
-            Integer val2 = p2.val;
-            if(val1!=val2)
-                return val1.compareTo(val2);
-            else
-                return key1.compareTo(key2);
-        }
-    }
-
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<Pair> q = new PriorityQueue<>(new PairComparator()); 
+        int[][] strengthRow = new int[mat.length][2];
         for(int i=0; i<mat.length; i++){
-            int count = 0;
-            for(int j=0; j<mat[0].length; j++){
-                if(mat[i][j] == 1)
-                    count++;
-                else
-                    break;
+            int strength =0;
+            for(int val: mat[i]){
+                strength += val;
             }
-            q.add(new Pair(i, count));
+            strengthRow[i][0] = strength;
+            strengthRow[i][1] =i;
         }
-        int[] ans = new int[k];
+        Arrays.sort(strengthRow , (a,b)->a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+        int[] result = new int[k];
         for(int i=0; i<k; i++){
-            ans[i] = q.poll().key;
+            result[i] = strengthRow[i][1];
         }
-
-        return ans;
+        return result;
     }
 }
