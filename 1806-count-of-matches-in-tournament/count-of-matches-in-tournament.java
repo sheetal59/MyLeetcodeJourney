04bcd9b736c8fb464712eba8515cabee26c2cad5
Sheetal <88383407+sheetal59@users.noreply.github.com>
2023-12-05 19:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (40.19%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,5 +1,11 @@
 class Solution {
     public int numberOfMatches(int n) {
-        return n-1;
+       // return n-1;
+        int matchesPlayed=0;
+        while(n>1){
+            matchesPlayed += n/2;
+            n = (n+1)/2;
+        }
+        return matchesPlayed;
     }
 }
