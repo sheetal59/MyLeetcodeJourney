500e0d72a0af692724e15a955e0b47f392e38543
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-01-12 12:31:17 +0530

    Time: 7 ms (33.95%) | Memory: 41.8 MB (28.59%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int count = 0;
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('I');
+        set.add('O');
+        set.add('U');
+        for(int i=0; i<s.length(); i++){
+            if(i<s.length()/2 && set.contains(s.charAt(i))){
+                count++;
+            }else if(i>= s.length()/2 && set.contains(s.charAt(i))){
+                count--;
+            }
+            if(count<0){
+                return false;
+            }
+        }
+        return count ==0;
+    }
+}
