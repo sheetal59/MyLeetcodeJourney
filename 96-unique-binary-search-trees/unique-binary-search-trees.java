17d46edbc5556b265d0aabcb862ba68c0c057f19
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-02-10 10:37:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (49.45%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,22 +1,13 @@
 class Solution {
     public int numTrees(int n) {
         int dp[]= new int[n+1];
-        Arrays.fill(dp,-1);
-        return numCat(n,dp);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
     }
-    public int numCat(int n, int dp[]){
-        if(n==0 || n==1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int ans=0;
-        for(int i=0; i<n; i++){
-            ans += numCat(i,dp) * numCat(n-i-1, dp);
-        }
-
-        return dp[n]=ans;
-    }
-
 }
