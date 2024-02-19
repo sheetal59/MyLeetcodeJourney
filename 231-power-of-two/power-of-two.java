d52f695fd810660374868557c51893d1d0352aed
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-02-19 10:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (56.93%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        //base case
+        if(n==1){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        if(n%2!=0){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+    }
+}
