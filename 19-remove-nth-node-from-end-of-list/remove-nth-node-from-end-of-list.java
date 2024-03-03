4c382b2fdf4a723881abf0290e0def1279a546c9
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-03-03 10:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (62.87%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,28 +10,24 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        //calculating the size of LinkedList
-        int sz =0;
+        int size =0;
         ListNode temp = head;
         while(temp!=null){
-            temp =  temp.next;
-            sz++;
+            temp=temp.next;
+            size++;
         }
-        //covering the corner case when only head node is there
-        if(n==sz){
+        if(n==size){
             head = head.next;
-             return head;
+            return head;
         }
-        //removing case
         int i=1;
-        int indx = sz-n; //we have to find index of prev node
-        ListNode prev =head;
-        while(i<indx){
+        int j = size-n;
+        ListNode prev = head;
+        while(i<j){
             prev = prev.next;
             i++;
-        }       
-     prev.next = prev.next.next;
-     return head;
-   
+        }
+        prev.next = prev.next.next;
+        return head;
     }
 }
