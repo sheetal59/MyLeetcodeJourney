4f64a00674b23ccea2895a4a4b92645e6a7ae4e8
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-04-09 12:08:10 +0530

    Time: 11 ms (17.22%) | Memory: 43.8 MB (20.55%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,25 +1,21 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         Queue<Integer> q = new LinkedList<>();
-
-        for ( int i = 0; i < tickets.length; i++){
-           q.add(i);
+        int time =0;
+        for(int i=0; i<tickets.length; i++){
+            q.add(i);
         }
-        int count = 0;
         while(!q.isEmpty()){
-            ++count;
-            int front = q.poll();
-            if(tickets[front] >= 1){
-                tickets[front] -= 1;
+            int front = q.remove();
+            tickets[front]--;
+            time++;
+            if(tickets[front]==0 && front==k){
+                return time;
             }
-            if(k == front && tickets[front] == 0){
-                break;
+            if(tickets[front]>0){
+                q.add(front);
             }
-            if(k != front && tickets[front] == 0){
-                continue;
-            }
-            q.add(front);
         }
-        return count;
+        return time;
     }
 }
