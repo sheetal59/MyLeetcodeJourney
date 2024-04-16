a2680958cf6f2b6867c6372539bed1041160ee25
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-04-16 13:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (44.89%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -18,10 +18,9 @@ class Solution {
         if(p==null && q==null){
             return true;
         }
-        if(p==null||q==null || p.val!=q.val){
+        if(p==null || q==null || p.val!=q.val){
             return false;
-        }    
+        }
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-        
     }
 }
