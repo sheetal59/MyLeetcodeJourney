f47d565341ee720fc66452eea873c70060656f30
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-05-12 20:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (23.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int fib(int n) {
         int dp[] = new int[n+1];
-        return fibn(n,dp);
+        return fibb(n,dp);
     }
-    public int fibn(int n, int dp[]){
+    public int fibb(int n, int dp[]){
         if(n==0 || n==1){
             return n;
         }
-        if(dp[n] !=0){
+        if(dp[n]!=0){
             return dp[n];
         }
-        dp[n] = fibn(n-1,dp) + fibn(n-2,dp);
+        dp[n] = fibb(n-1,dp) + fibb(n-2,dp);
         return dp[n];
     }
 }
