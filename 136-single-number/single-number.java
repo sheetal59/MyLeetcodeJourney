4a82735b82620e77576bf65dcb0f208f21cb4e44
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-06-03 15:27:28 +0530

    Time: 14 ms (13.72%) | Memory: 44.5 MB (97.09%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        for(int num : map.keySet()){
+            if(map.get(num)==1){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
