a8e3deb2ab4d89c0fda8cdffada7da3b3761287f
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-06-05 10:40:27 +0530

    Time: 6 ms (40.73%) | Memory: 42.9 MB (65.61%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.java
@@ -0,0 +1,16 @@
+class Solution {
+        public List<String> commonChars(String[] A) {
+        List<String> ans = new ArrayList<>();
+        int[] count = new int[26]; 
+        Arrays.fill(count, Integer.MAX_VALUE);
+        for (String str : A) {
+            int[] cnt = new int[26];
+            str.chars().forEach(c -> ++cnt[c - 'a']); // count each char's frequency in string str.
+            for (int i = 0; i < 26; ++i) { count[i] = Math.min(cnt[i], count[i]); } // update minimum frequency.
+        }
+        for (char c = 'a'; c <= 'z'; ++c) {
+            while (count[c - 'a']-- > 0) { ans.add("" + c); }
+        }
+        return ans;
+    }
+}
