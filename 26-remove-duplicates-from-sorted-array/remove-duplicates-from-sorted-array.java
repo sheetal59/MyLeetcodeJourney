054578bb1fc87779901637b412f911a4b1ad5c1d
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-06-27 22:48:16 +0530

    Time: 2 ms (13.32%) | Memory: 44.9 MB (17.51%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int i=0;
+        for(int j=0; j<nums.length; j++){
+            if(set.contains(nums[j])){
+                continue;
+            }else{
+                set.add(nums[j]);
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
