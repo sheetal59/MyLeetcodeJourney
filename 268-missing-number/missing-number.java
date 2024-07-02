c083e80c8cb3f9097622de8430c33e4dac5cbf07
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-07-02 11:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (43.01%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,21 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int range = nums.length;
-        //Initialize the sum of all elements of array with 0
-        int numsSum = 0;
-        //Initialize the sum of all the numbers from 0 to nums.length
-        int rangeSum = 0;
-        //iterate through the numbers of range and update the value of rangeSum
-        for(int i=0; i<=range; i++){
+        int rangeSum=0;
+        int numSum =0;
+        for(int i=0; i<=range;i++){
             rangeSum += i;
         }
-        //iterate through each element in array and get their sum
-        for(int num:nums){
-            numsSum += num;
+        for(int i=0 ; i<nums.length; i++){
+            numSum += nums[i];
         }
-        //missing no. =  RangeSum - numsSum;
-        int missingNo = rangeSum - numsSum;
-        return missingNo;
-        
+        int missingNum = rangeSum - numSum;
+        return missingNum;
     }
 }
