cb75125aca4273f6c53b5fb2a688685fac8b8060
Sheetal <88383407+sheetal59@users.noreply.github.com>
2024-07-07 20:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (54.33%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+       int consumed =0;
+       while(numBottles>= numExchange){
+        consumed += numExchange;
+        numBottles -= numExchange;
+        numBottles ++;
+       }
+       return consumed + numBottles;
+    }
+}
